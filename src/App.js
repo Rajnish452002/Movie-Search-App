abd4ebfbf2349bc6e701dd36bac7b65eb9365c2b
Rajnish <rajnishkkrr@gmail.com>
2023-07-05 09:44:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+import { useEffect, useState } from 'react';
+
+
+import SearchIcon from './search.svg';
+import Moviecard from './Moviecard';
+
+
+const API_URL = '  http://www.omdbapi.com/?i=tt3896198&apikey=237fb55e '
+
+const movie1 =
+{
+  "Title": "The Amazing Spiderman 2 Webb Cut",
+  "Year": "2021",
+  "imdbID": "tt18351128",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
+}
+
+
+
+
 function App() {
+
+  const [movies, setmovie] = useState([])
+  const [searchterm , setsearchterm] = useState('')
+
+
+  const searchmovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    setmovie(data.Search);
+  }
+
+  useEffect(() => {
+    searchmovie('Spiderman')
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+
+      <h1>Movie Arena</h1>
+
+      <div className='search'>
+        <input placeholder='Search for Movie'
+          value={searchterm}
+          onChange={(e) => setsearchterm(e.target.value)
+          }
+
+        />
+
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchmovie(searchterm) }
+        />
+
+
+      </div>
+
+      {
+        movies?.length > 0 ? (
+          <div className='container'>
+            {
+              movies.map((movie)=>(
+                <Moviecard movie={movie} />
+              ))
+            }
+          </div>
+
+        ) 
+        :
+        (
+          <div className='empty' >
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
+
+
+
     </div>
   );
 }
